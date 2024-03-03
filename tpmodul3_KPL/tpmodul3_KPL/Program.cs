faef9cee3cba7bdf93f5cb2dcb6db3ba18014b9a
brdwja <103436437+brdwja@users.noreply.github.com>
2024-03-04 04:56:19 +0700

    Add files via upload

---
+++ tpmodul3_KPL/tpmodul3_KPL/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, World!");
+private void buttonSubmit_Click(object sender, EventArgs e)
+{
+    // Membaca teks dari TextBox input
+    string inputText = textBoxInput.Text;
+
+    // Memeriksa apakah inputText adalah "NAMA_PRAKTIKAN"
+    if (inputText.Equals("NAMA_PRAKTIKAN"))
+    {
+        // Menetapkan teks label output
+        labelOutput.Text = "Halo NAMA_PRAKTIKAN";
+    }
+    else
+    {
+        // Jika input tidak sesuai, menetapkan teks label output ke pesan error
+        labelOutput.Text = "Input tidak valid!";
+    }
+}
